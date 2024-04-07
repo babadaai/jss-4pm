bd277fa448865939b5301bd52770fed74494a79c
Nischal Mainali <nischalmaianli@gmail.com>
2024-04-07 16:12:55 +0545

    day 1

---
+++ day-1/index.js
@@ -0,0 +1,25 @@
+console.log("hello world");
+// variable
+/*
+multi line comment 
+*/
+//  variable start
+//  ley words: var , const , let
+const person = "ok";
+const person1 = "okkkk";
+
+let number = 1;
+number = 5;
+// print the number
+console.log(number);
+console.log({ person });
+console.log({ person1 });
+// interaction
+alert("hello world");
+const question1 = prompt("what is your name");
+
+const question = confirm("are you 18 years old");
+
+console.log({ question1, question });
+// update the above question to print name and age
+alert("your name is " + question1 + " your age " + question);
---
+++ day-1/product.js
@@ -0,0 +1,6 @@
+//  write a multiplication calculation for 3 digits calculator.
+const num1 = prompt("enter 1st number");
+const num2 = prompt("enter 2nd number");
+const num3 = prompt("enter 3rd number");
+let product = num1 * num2 * num3;
+alert("your product =" + product);
